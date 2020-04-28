b0ba428314729c502fdbc7aea4c763468206713b
Heltonphg <wylammi.helton@gmail.com>
2020-04-28 12:14:02 -0300

    correção de autenticação

--- app/Controllers/Http/SessionController.js
+++ app/Controllers/Http/SessionController.js
@@ -1,10 +1,25 @@
 'use strict'
+const User = use('App/Models/User')
 
 class SessionController {
   async store({ request, response, auth }) {
-    const { email, password } = request.all()
-    const token = await auth.attempt(email, password)
-    return token
+    try {
+      const { email, password } = request.all()
+
+      const usr = await User.findByOrFail('email', email)
+      const { id, first_name, last_name } = usr
+
+      if (usr) {
+        const token = await auth.attempt(email, password)
+        const user = {
+          token,
+          user_info: { id, first_name, last_name }
+        }
+        return user
+      }
+    } catch (error) {
+      return response.status(error.status).json({ error: { message: "Impossível Autentiucar-se com as credenciasi fornecidas" } })
+    }
   }
 }
 

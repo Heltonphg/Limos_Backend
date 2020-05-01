d3301d50ecefcb66a7036b5b0e3445c9c42e462d
Heltonphg <wylammi.helton@gmail.com>
2020-05-01 11:07:00 -0300

    crud category

--- app/Controllers/Http/CategoryController.js
+++ app/Controllers/Http/CategoryController.js
@@ -4,28 +4,36 @@ const Category = use('App/Models/Category')
 
 class CategoryController {
 
-  async index({ request, response, view }) {
+  async index({ }) {
     const categories = await Category.all()
     return categories
   }
 
 
-  async store({ request, response }) {
+  async store({ request }) {
     const name = request.only(['name'])
     const category = await Category.create(name)
     return category
 
   }
 
-
-  async show({ params, request, response, view }) {
+  async show({ params }) {
+    const category = await Category.findOrFail(params.id)
+    return category
   }
 
 
-  async update({ params, request, response }) {
+  async update({ params, request }) {
+    const name = request.only(['name'])
+    const category = await Category.findOrFail(params.id)
+    category.merge(name)
+    await category.save()
+    return category
   }
 
   async destroy({ params, request, response }) {
+    const category = await Category.findOrFail(params.id)
+    await category.delete()
   }
 
 }

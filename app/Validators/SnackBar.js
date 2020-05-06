5aa4ce6f7e8dc3200378c300496afd22e251db30
Heltonphg <wylammi.helton@gmail.com>
2020-05-06 16:54:52 -0300

    validation snackBar

---
+++ app/Validators/SnackBar.js
@@ -0,0 +1,21 @@
+'use strict'
+const Antl = use('Antl')
+
+class SnackBar {
+  get validateAll() {
+    return true
+  }
+
+  get rules() {
+    return {
+      name: 'required|unique:snack_bars',
+      payment: 'required'
+    }
+  }
+
+  get messages() {
+    return Antl.list('validation')
+  }
+}
+
+module.exports = SnackBar
